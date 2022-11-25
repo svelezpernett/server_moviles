bb91cf59226742a6151c764c727db7af341e725d
Santiago Velez Pernett <89142690+svelezpernett@users.noreply.github.com>
2022-11-24 21:04:54 -0500

    Update routes.js

--- routes.js
+++ routes.js
@@ -17,10 +17,10 @@ router.post ("/vendedor",[
 router.get("/vendedor",controller.getVendedor)
 
 router.post("/venta",[
-        check("idvend","el idvend es obligatorio").not(),
-        check("zona","la zona es obligatorio").matches(/^[a-zA-Z]+$/),
-        check("fecha","fecha es obliogatorio").isDate(),
-        check("valorventa","valorventa es obligatorio").not().isEmpty()
+        check("idvend","el idvend es obligatorio"),
+        check("zona","la zona es obligatorio"),
+        check("fecha","fecha es obliogatorio"),
+        check("valorventa","valorventa es obligatorio")
 ],
 ValidarCampos
  ,controller.postVenta)
